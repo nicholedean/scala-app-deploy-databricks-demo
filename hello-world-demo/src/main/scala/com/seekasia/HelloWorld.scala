f9e97ad4ff229e37b66c2de721a6633e38a2ee59
Nik Yim <yimkh@seekasia.com>
2016-08-22 19:46:30 +0800

    test Jenkins automate build

--- hello-world-demo/src/main/scala/com/seekasia/HelloWorld.scala
+++ hello-world-demo/src/main/scala/com/seekasia/HelloWorld.scala
@@ -4,6 +4,6 @@ object HelloWorld {
 
   def main(args: Array[String]): Unit = {
     println("Hello, world! Yohohohoh~")
-    println("Version 1.0.1 updated~, test GitHUb webhook")
+    println("Version 1.0.1 updated~, test GitHUb poll SCM, muahahah")
   }
 }
