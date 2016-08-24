02cf84154014cdf4002a76459fdaad13c8b76b37
Nik Yim <yimkh@seekasia.com>
2016-08-24 22:57:37 +0800

    hello-world-demo ver1.0.3

--- hello-world-demo/src/main/scala/com/seekasia/HelloWorld.scala
+++ hello-world-demo/src/main/scala/com/seekasia/HelloWorld.scala
@@ -6,6 +6,6 @@ object HelloWorld {
 
     println("Continuous Integration for Databricks POC DONE!! Can implement right away~!")
     println("New version updated. Jenkins help you do the rest!!, All you need to do is update the version and commit to develop/master")
-
+    println("It just make thing so much easier than you thought!!")
   }
 }
