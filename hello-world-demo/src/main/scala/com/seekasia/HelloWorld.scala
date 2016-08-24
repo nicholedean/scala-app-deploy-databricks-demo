92bf9623201cd323e8d762a5d8e57a7301d5de75
Nik Yim <yimkh@seekasia.com>
2016-08-24 16:54:53 +0800

    test jar contents

--- hello-world-demo/src/main/scala/com/seekasia/HelloWorld.scala
+++ hello-world-demo/src/main/scala/com/seekasia/HelloWorld.scala
@@ -3,7 +3,7 @@ package com.seekasia
 object HelloWorld {
 
   def main(args: Array[String]): Unit = {
-    println("Hello, world! Yohohohoh~")
+
     println("Version 1.0.1 updated~, test GitHUb poll SCM, muahahah")
   }
 }
