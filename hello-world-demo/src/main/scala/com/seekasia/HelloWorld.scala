08867c8e0987ac6065acd6d26693c5da1ec6a11c
Nik Yim <yimkh@seekasia.com>
2016-08-24 21:50:07 +0800

    refactor pom.xml for maven compile (jar with class)

--- hello-world-demo/src/main/scala/com/seekasia/HelloWorld.scala
+++ hello-world-demo/src/main/scala/com/seekasia/HelloWorld.scala
@@ -5,5 +5,7 @@ object HelloWorld {
   def main(args: Array[String]): Unit = {
 
     println("Version 1.0.1 updated~, test GitHUb poll SCM, muahahah")
+    println("Finally I found it.. gan~")
+    println("And I solve it!!!")
   }
 }
