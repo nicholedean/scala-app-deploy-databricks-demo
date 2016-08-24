b4cd0ed661e6b884c13162fb145c53072ab8156d
Nik Yim <yimkh@seekasia.com>
2016-08-24 22:43:44 +0800

    hello-world-demo ver1.0.2

--- hello-world-demo/src/main/scala/com/seekasia/HelloWorld.scala
+++ hello-world-demo/src/main/scala/com/seekasia/HelloWorld.scala
@@ -5,5 +5,7 @@ object HelloWorld {
   def main(args: Array[String]): Unit = {
 
     println("Continuous Integration for Databricks POC DONE!! Can implement right away~!")
+    println("New version updated. Jenkins help you do the rest!!, All you need to do is update the version and commit to develop/master")
+
   }
 }
