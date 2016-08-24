c5fe5ff3a59d4da61c9cc414c311f76dd6bc6824
Nik Yim <yimkh@seekasia.com>
2016-08-24 22:30:06 +0800

    update hello-world print statement

--- hello-world-demo/src/main/scala/com/seekasia/HelloWorld.scala
+++ hello-world-demo/src/main/scala/com/seekasia/HelloWorld.scala
@@ -4,8 +4,6 @@ object HelloWorld {
 
   def main(args: Array[String]): Unit = {
 
-    println("Version 1.0.1 updated~, test GitHUb poll SCM, muahahah")
-    println("Finally I found it.. gan~")
-    println("And I solve it!!!")
+    println("Continuous Integration for Databricks POC DONE!! Can implement right away~!")
   }
 }
